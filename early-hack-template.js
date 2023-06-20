d885768b0081d81bc1ff997f6098a760e4a1a580
Ryan Howe <ryhowe@gmail.com>
2023-06-20 07:10:19 -0400

    Make targets randomized

--- early-hack-template.js
+++ early-hack-template.js
@@ -1,24 +1,33 @@
-/** @param {NS} ns */
 export async function main(ns) {
-  const target = "foodnstuff";
+
+  var targets = ["nectar-net",
+    "omega-net",
+    "iron-gym",
+    "phantasy",
+    "harakiri-sushi",
+    "silver-helix",
+    "max-hardware",
+    "foodnstuff",
+    "neo-net",
+    "zer0",
+    "sigma-cosmetics",
+    "joesguns"]
+
+  var target = targets[Math.floor(Math.random() * targets.length)];
   const moneyThresh = ns.getServerMaxMoney(target) * 0.75;
   const securityThresh = ns.getServerMinSecurityLevel(target) + 5;
 
-  if (ns.fileExists("BruteSSH.exe", "home")) {
-    ns.brutessh(target);
-  }
-
-  ns.nuke(target);
-
-
   while (true) {
     if (ns.getServerSecurityLevel(target) > securityThresh) {
+      // If the server's security level is above our threshold, weaken it
       await ns.weaken(target);
     }
     else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+      // If the server's money is less than our threshold, grow it
       await ns.grow(target);
     }
     else {
+      // Otherwise, hack it
       await ns.hack(target);
     }
   }
