020613514d4e785570940fed1c9321cc7a498f83
Ryan Howe <ryhowe@gmail.com>
2023-06-26 20:40:05 -0400

    Fix logic for tk

--- take-over.js
+++ take-over.js
@@ -4,27 +4,29 @@ export async function main(ns) {
 
   var targets = multiscan(ns, 'home');
 
-
   for (let i = 0; i < targets.length; i++) {
-
     let server = targets[i]
-    if (ns.fileExists('BruteSSH.exe', "home")) {
-      ns.brutessh(server);
+    if (ns.hasRootAccess(server) == false) {
+      
+      if (ns.fileExists('BruteSSH.exe', "home")) {
+        ns.brutessh(server);
+      }
+      if (ns.fileExists('FTPCrack.exe', "home")) {
+        ns.ftpcrack(server);
+      }
+      if (ns.fileExists('relaySMTP.exe', "home")) {
+        ns.relaysmtp(server);
+      }
+      if (ns.fileExists('HTTPWorm.exe', "home")) {
+        ns.httpworm(server);
+      }
+      if (ns.fileExists('SQLInject.exe', "home")) {
+        ns.sqlinject(server);
+      }
+      if (ns.getServerNumPortsRequired(server) == true){
+        ns.nuke(server)
+      }
     }
-    if (ns.fileExists('FTPCrack.exe', "home")) {
-      ns.ftpcrack(server);
-    }
-    if (ns.fileExists('relaySMTP.exe', "home")) {
-      ns.relaysmtp(server);
-    }
-    if (ns.fileExists('HTTPWorm.exe', "home")) {
-      ns.httpworm(server);
-    }
-    if (ns.fileExists('SQLInject.exe', "home")) {
-      ns.sqlinject(server);
-    }
-
-    ns.nuke(server);
   }
 
 }
